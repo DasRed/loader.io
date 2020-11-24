5dbe678a4196f18606d0c2558521585ef78465e6
Marco Starker <marco.starker@gmx.net>
2020-11-24 09:26:51 +0100

    feat: create the version

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,10 @@
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-
-// https://rollupjs.org/guide/en#big-list-of-options
-export default [
-    // Node Module
-    {
-        input:   './src/index.js',
-        output:  {
-            file:    './dist/index.js',
-            format:  'cjs',
-            name:    'LoaderIO',
-            exports: 'named',
-        },
-        plugins: [
-            resolve(),
-            commonjs(),
-        ]
-    },
-];
+export default {
+    input:  './src/index.js',
+    output: {
+        file:      './dist/index.js',
+        format:    'cjs',
+        name:      'LoaderIO',
+        exports:   'named',
+        sourcemap: false,
+    }
+};
