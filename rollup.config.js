72a76f6d2b23a7eab56ca8a4e1de88c0e9545163
Marco Starker <marco.starker@gmx.net>
2020-11-30 15:27:59 +0100

    fix: fixing build of release

--- rollup.config.js
+++ rollup.config.js
@@ -28,5 +28,16 @@ export default {
                 },
             ]
         }),
+        {
+            renderChunk: function (source) {
+                const matches = /(exports(?:\['default']|\.default)) = (.*);/gi.exec(source);
+                if (matches) {
+                    const pos = source.indexOf('exports.');
+
+                    source = source.substring(0, pos) + `module.exports = exports = ${matches[2]};\n\n` + source.substring(pos);
+                }
+                return source;
+            }
+        },
     ]
 };
