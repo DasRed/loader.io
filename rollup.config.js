b797777ad82d4edb278b7506d3e8303a936d718c
Marco Starker <marco.starker@gmx.net>
2020-11-30 14:41:13 +0100

    fix: fixing build of release

--- rollup.config.js
+++ rollup.config.js
@@ -29,22 +29,22 @@ export default {
                 },
             ]
         }),
-        babel({babelHelpers: 'bundled'}),
-        {
-            renderChunk: function (source) {
-                const lines = source.split('\n');
-                for (let i = 0; i < lines.length; i++) {
-                    const line    = lines[i];
-                    const matches = /^(exports(?:\['default']|\.default)) = (.*);$/.exec(line);
-                    if (matches) {
-                        lines[i] = 'module.exports = exports = ' + matches[2] + ';\n' +
-                            'Object.defineProperty(exports, "__esModule", { value: true });\n' +
-                            matches[1] + ' = exports;';
-                        break;
-                    }
-                }
-                return lines.join('\n');
-            }
-        }
+        //babel({babelHelpers: 'bundled'}),
+        //{
+        //    renderChunk: function (source) {
+        //        const lines = source.split('\n');
+        //        for (let i = 0; i < lines.length; i++) {
+        //            const line    = lines[i];
+        //            const matches = /^(exports(?:\['default']|\.default)) = (.*);$/.exec(line);
+        //            if (matches) {
+        //                lines[i] = 'module.exports = exports = ' + matches[2] + ';\n' +
+        //                    'Object.defineProperty(exports, "__esModule", { value: true });\n' +
+        //                    matches[1] + ' = exports;';
+        //                break;
+        //            }
+        //        }
+        //        return lines.join('\n');
+        //    }
+        //}
     ]
 };
