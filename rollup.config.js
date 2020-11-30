56878a97be11dc57a885e5bbda552833fccb6a80
Marco Starker <marco.starker@gmx.net>
2020-11-30 09:41:06 +0100

    build: adding types from @geopic to the build. :)

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,28 @@
+import copy from 'rollup-plugin-copy'
+import clear from 'rollup-plugin-clear'
+
 export default [
     {
-        input:  './src/index.js',
-        output: {
+        input:    './src/index.js',
+        output:   {
             dir:                 './dist/',
             format:              'cjs',
             exports:             'named',
             sourcemap:           false,
             preserveModules:     true,
             preserveModulesRoot: 'src',
-        }
+        },
+        external: ['node-fetch', 'qs'],
+        plugins:  [
+            clear({targets: ['./dist/']}),
+            copy({
+                targets: [
+                    {
+                        src:  './src/index.d.ts',
+                        dest: './dist'
+                    },
+                ]
+            })
+        ]
     },
-    //{
-    //    input:  './src/index.js',
-    //    output: {
-    //        file:      './dist/index.js',
-    //        format:    'cjs',
-    //        name:      'LoaderIO',
-    //        exports:   'named',
-    //        sourcemap: false,
-    //    }
-    //},
 ];
